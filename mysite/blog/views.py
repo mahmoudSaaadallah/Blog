8f91f39b130c18407f76cbeceb9810f3d6fdc390
Mahmoud <mahmoud.saadallah73@gmail.com>
2026-01-02 16:33:48 +0200

    Fixing the context to be dict not a tupel

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,4 +9,4 @@ def post_list(request):
 
 def post_detail(request, id):
     post = get_object_or_404(Post, pk=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post', post})
+    return render(request, 'blog/post/detail.html', {'post': post})
