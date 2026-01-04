8c92f7e98278e96a28316a503427102f54ff8d6c
Mahmoud <mahmoud.saadallah73@gmail.com>
2026-01-04 14:28:41 +0200

    Handling the sending mail

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,4 +5,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', view=views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', view=views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
